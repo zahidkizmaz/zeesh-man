24d804d0454778430deebfd4b3b84dca02a704dd
Zahid Kizmaz <tech@zahid.rocks>
2024-03-14 19:26:40 +0100

    feat: add optional logging

--- tests/basic.zshrc
+++ tests/basic.zshrc
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+export ZEESH_DEBUG=1
 source /zeesh/zeesh.zsh
 
 zeesh_get "jeffreytse/zsh-vi-mode"
--- zeesh.zsh
+++ zeesh.zsh
@@ -2,6 +2,7 @@
 set -eo pipefail
 IFS=$'\n\t'
 
+ZEESH_DEBUG=${ZEESH_DEBUG:-0}
 ZEESH_DRY_RUN=${ZEESH_DRY_RUN:-0}
 DATA_HOME=${XDG_DATA_HOME:-"$HOME/.local/share"}
 PLUGIN_DIR="$DATA_HOME/zeesh"
@@ -14,7 +15,7 @@ function _command() {
   local cmd=$1
 
   if [[ $ZEESH_DRY_RUN -eq 1 ]]; then
-    echo "WARNING: zeehs is in dry run mode! Commands will not be run."
+    echo "WARNING: zeehs is in dry run mode! Commands will not be executed."
     echo "$cmd"
     return 0
   fi
@@ -22,9 +23,15 @@ function _command() {
   return 0
 }
 
+function _log() {
+  if [[ $ZEESH_DEBUG -eq 1 ]]; then
+    echo "$1"
+  fi
+}
+
 function _create_plugin_dir() {
   if [[ ! -d $PLUGIN_DIR ]]; then
-    echo "WARNING: $PLUGIN_DIR doesn't exists. Creating directory: $PLUGIN_DIR."
+    _log "WARNING: $PLUGIN_DIR doesn't exists. Creating directory: $PLUGIN_DIR."
     _command "mkdir -p $PLUGIN_DIR"
   fi
   return 0
@@ -35,7 +42,7 @@ function _download_plugin() {
   local plugin_folder_name="${2}"
   local plugin_url="$GITHUB_URL$plugin_name.git"
 
-  echo "INFO: Downloading plugin: $plugin_name"
+  _log "INFO: Downloading plugin: $plugin_name"
   _command "$GIT_DOWNLOAD_COMMAND $plugin_url $PLUGIN_DIR/$plugin_folder_name"
   return 0
 }
@@ -45,22 +52,22 @@ function _source_plugin() {
   local repo_name="${2}"
 
   if [[ ! -f $plugin_location/$.plugin.zsh ]]; then
-    echo "DEBUG: sourcing $plugin_location/$repo_name.plugin.zsh"
+    _log "DEBUG: sourcing $plugin_location/$repo_name.plugin.zsh"
     _command "source $plugin_location/$repo_name.plugin.zsh"
     SOURCED_PLUGINS+=($plugin_location)
     return 0
   elif [[ ! -f $plugin_location/$.zsh ]]; then
-    echo "DEBUG: sourcing $plugin_location/$repo_name.zsh"
+    _log "DEBUG: sourcing $plugin_location/$repo_name.zsh"
     _command "source $plugin_location/$repo_name.zsh"
     SOURCED_PLUGINS+=($plugin_location)
     return 0
   fi
-  echo "WARNING: could find anything to source in $plugin_location"
+  _log "WARNING: could find anything to source in $plugin_location"
   return 1
 }
 
 function setup() {
-  echo "DEBUG: zeesh initializing..."
+  _log "DEBUG: zeesh initializing..."
   _create_plugin_dir
   return 0
 }
@@ -72,16 +79,16 @@ function zeesh_get() {
   local plugin_location="$PLUGIN_DIR/${user_and_repo_name[2]}"
 
   if [[ ! -d $plugin_location ]]; then
-    echo "DEBUG: $plugin doesn't exists. Downloading..."
+    _log "DEBUG: $plugin doesn't exists. Downloading..."
     _download_plugin "$plugin" "${user_and_repo_name[2]}"
   fi
 
   if [[ ${SOURCED_PLUGINS[@]} =~ $plugin_location ]]; then
-    echo "DEBUG: Already sourced $plugin_location."
+    _log "DEBUG: Already sourced $plugin_location."
     return 0
   fi
   _source_plugin "$plugin_location" "${user_and_repo_name[2]}"
-  echo "DEBUG: Sourced plugins: ${SOURCED_PLUGINS[@]}"
+  _log "DEBUG: Sourced plugins: ${SOURCED_PLUGINS[@]}"
   return 0
 }
 
